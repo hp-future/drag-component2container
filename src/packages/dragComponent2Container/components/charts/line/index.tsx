5b0119cb82d6f7101fe5dd12a895d8dc424ce42e
huangpu <huangpu@ysstech.com>
2023-03-28 11:58:51 +0800

    封装chart缩放事件

--- src/packages/dragComponent2Container/components/charts/line/index.tsx
+++ src/packages/dragComponent2Container/components/charts/line/index.tsx
@@ -11,7 +11,7 @@ const LineChart = ({ props }: { props: DataType }) => {
   const [showHandle, setShowHandle] = useState(false);
 
   // 调整 chart 大小
-  useResize(showHandle, props.key);
+  const { mouseDownEvent } = useResize(showHandle);
 
   useEffect(() => {
     asyncFetch();
@@ -49,6 +49,10 @@ const LineChart = ({ props }: { props: DataType }) => {
     });
   }, [configData]);
 
+  useEffect(() => {
+    setShowHandle(true);
+  }, [configData.currentChart.key]);
+
   // 单击查看配置选项
   function clickEvent(e: React.MouseEvent) {
     dispatch(updateCurrentChart(configData.data.find((item) => item.key === props.key)!));
@@ -56,17 +60,28 @@ const LineChart = ({ props }: { props: DataType }) => {
   }
 
   return (
-    <div className="linechart-container" onClick={clickEvent} style={props.containerProps}>
+    <div className="linechart-container" onClick={clickEvent} style={props.containerProps} id={props.key}>
       <Line {...config} />
-      <div className="out-handle" style={{ display: showHandle ? 'block' : 'none' }}>
-        <div className="handle top-left"></div>
-        <div className="handle top-center"></div>
-        <div className="handle top-right"></div>
-        <div className="handle right"></div>
-        <div className="handle bottom-right"></div>
-        <div className="handle bottom-center"></div>
-        <div className="handle bottom-left"></div>
-        <div className="handle left"></div>
+      <div
+        className="out-handle"
+        style={{
+          display: showHandle && props.key === configData.currentChart.key ? 'block' : 'none',
+          backgroundColor: showHandle && props.key === configData.currentChart.key ? 'rgb(255, 0, 0, 0.1)' : '',
+        }}
+        onMouseDown={mouseDownEvent}
+      >
+        <div onClick={(e) => e.stopPropagation()} data-handle-name="top-left" className="handle top-left"></div>
+        <div onClick={(e) => e.stopPropagation()} data-handle-name="top-center" className="handle top-center"></div>
+        <div onClick={(e) => e.stopPropagation()} data-handle-name="top-right" className="handle top-right"></div>
+        <div onClick={(e) => e.stopPropagation()} data-handle-name="right-center" className="handle right-center"></div>
+        <div onClick={(e) => e.stopPropagation()} data-handle-name="bottom-right" className="handle bottom-right"></div>
+        <div
+          onClick={(e) => e.stopPropagation()}
+          data-handle-name="bottom-center"
+          className="handle bottom-center"
+        ></div>
+        <div onClick={(e) => e.stopPropagation()} data-handle-name="bottom-left" className="handle bottom-left"></div>
+        <div onClick={(e) => e.stopPropagation()} data-handle-name="left-center" className="handle left-center"></div>
       </div>
     </div>
   );
